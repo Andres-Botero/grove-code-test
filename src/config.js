f056bdd17248d2ca6934ac9488a9a657a76a238b
Andres Botero <anmabot@gmail.com>
2018-08-20 13:35:18 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,9 +4,6 @@ export default {
             baseUrl: 'https://scheduler-challenge.herokuapp.com/'
         }
     },
-    pagination: {
-        itemsPerPage:30
-    },
     layout: {
         break_small:768,
         break_large:1024
